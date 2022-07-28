e4530137797307ee30a5f5f3591f583c09063817
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-07-28 14:14:04 +1000

    install react-bootstrap bootstrap react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import BookingCar from "./pages/Booking";
 
 function App() {
   return (
     <div className="App">
-      <h1>Renting Cars</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/bookingcar" element={<BookingCar />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/DefaultLayout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DefaultLayout = () => {
+  return (
+    <div>
+      <div className="header"></div>
+      <div className="content"></div>
+    </div>
+  );
+};
+
+export default DefaultLayout;
