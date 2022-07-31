6c8f1f829c0ed604f074d8850e28de1a7e488ba7
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-07-31 10:25:12 +1000

    .

--- server/src/Router/RentalCarRouter.js
+++ server/src/Router/RentalCarRouter.js
@@ -1,16 +1,16 @@
 import express from "express";
 const route = express.Router();
 
-route.get("/", (req, res, next) => {
-  try {
-    res.json({
-      status: "success",
-      message: "todo get method",
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+// route.get("/", (req, res, next) => {
+//   try {
+//     res.json({
+//       status: "success",
+//       message: "todo get method",
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 route.post("/", (req, res, next) => {
   console.log(req.body);
