abafb62e01abee0c1febe731fe838d532f802811
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-08-21 11:43:29 +1000

    start working on home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import DefaultLayout from "../components/DefaultLayout";
 import { getCarDetailsAction } from "../redux/actions/carDetailsAction";
-
+import { Row, Col } from "antd";
 const HomePage = () => {
   const { carDetails } = useSelector((state) => state.carDetails);
 
@@ -14,8 +14,7 @@ const HomePage = () => {
 
   return (
     <DefaultLayout>
-      <h1>Home Page</h1>
-      <h1>The length of car is {carDetails.length}</h1>
+      <Row></Row>
     </DefaultLayout>
   );
 };
